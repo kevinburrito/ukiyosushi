05edc9dbec66404915191ad226adafb367ba005a
Kevin Bariya <flame112740@hotmail.com>
2019-12-28 12:41:04 +0430

    Update script.js

--- script.js
+++ script.js
@@ -24,19 +24,19 @@ function imgslider(x)
    switch(x){
       case 1:
          bg.style.animation = "fade .5s";
-         bg.style.backgroundImage = "-webkit-linear-gradient(rgba(0,0,0,.375), rgba(0,0,0,.375)),url(img/header.jpg)";
+         bg.style.backgroundImage = "-webkit-linear-gradient(rgba(0,0,0,.375), rgba(0,0,0,.375)),url(img/header.JPG)";
          caption.style.marginLeft = "0em";
          caption.innerHTML = "Chef's Special Sushi Platter";
          break;
       case 2: 
          bg.style.animation = "fade1 .5s"
-         bg.style.backgroundImage = "-webkit-linear-gradient(rgba(0,0,0,.375), rgba(0,0,0,.375)),url(img/pokebowl.jpg)";
+         bg.style.backgroundImage = "-webkit-linear-gradient(rgba(0,0,0,.375), rgba(0,0,0,.375)),url(img/pokebowl.JPG)";
          caption.style.marginLeft = "2em";
          caption.innerHTML = "Spring Poke Bowl";
          break;
       case 3:
          bg.style.animation = "fade2 .5s";
-         bg.style.backgroundImage = "-webkit-linear-gradient(rgba(0,0,0,.375), rgba(0,0,0,.375)),url(img/ramen.jpg)";
+         bg.style.backgroundImage = "-webkit-linear-gradient(rgba(0,0,0,.375), rgba(0,0,0,.375)),url(img/ramen.JPG)";
          caption.style.marginLeft = "3.5em";
          caption.innerHTML = "Miso Ramen"; 
    }
